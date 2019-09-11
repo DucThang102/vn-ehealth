17ad4c89241e0b7ea99ca13b2d1a2e9bce11e18c
tungdt <duongthanhtungvn01@gmail.com>
2019-09-11 14:26:37 +0700

    Update

--- vn-ehealth-service/src/main/java/vn/ehealth/service/auth/jwt/controller/JWTAuthController.java
+++ vn-ehealth-service/src/main/java/vn/ehealth/service/auth/jwt/controller/JWTAuthController.java
@@ -1,5 +1,6 @@
 package vn.ehealth.service.auth.jwt.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.ehealth.service.auth.jwt.JWTTokenProvider;
@@ -36,5 +39,30 @@ public class JWTAuthController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
         String jwt = tokenProvider.generateToken(authentication);
         return ResponseEntity.ok(Map.of("accessToken", jwt, "tokenType", "Bearer"));
-    }    
+    }
+    
+    @GetMapping("/get_permissions")
+    public ResponseEntity<?> getPermissions(@RequestParam String username) {
+        var permissions = List.of(
+                    Map.of(
+                            "tab", "tab_hsba",
+                            "items", List.of("hsba_chuaxuly", "hsba_daluu")
+                    ),
+                    
+                    Map.of(
+                            "tab", "tab_dm",
+                            "items", List.of("dm_dungchung", "dm_chuan_hoa")
+                    )
+                );
+        
+        return ResponseEntity.ok(permissions);
+    }
+    
+    @GetMapping("/check_page_permission")
+    public boolean checkPagePermission(@RequestParam String username, @RequestParam String page) {
+        if("test".equals(page)) {
+            return false;
+        }
+        return true;
+    }
 }
--- vn-ehealth-web-js/server.js
+++ vn-ehealth-web-js/server.js
@@ -1,11 +1,14 @@
 var express = require('express');
- var server = express();
- var ejs = require('ejs'); 
- ejs.open = '<%'; 
- ejs.close = '%>';
+var server = express();
+var ejs = require('ejs'); 
+var cookieParser = require('cookie-parser');
+var sync_req = require('sync-request');
+
+ejs.open = '<%'; 
+ejs.close = '%>';
 
 
- var oneDay = 86400000;
+var oneDay = 86400000;
  
 server.set("view options", {layout: false});  
 server.engine('html', require('ejs').renderFile); 
@@ -13,15 +16,28 @@ server.engine('js', require('ejs').renderFile);
 server.engine('css', require('ejs').renderFile); 
 server.set('view engine', 'html');
 server.set('views', __dirname + "/www");
+server.use(cookieParser());
 
 server.all("*", function(req, res, next) {
     var request = req.params[0];
     if(request == '/') request = '/index.html';
-    
+
     var pos = request.lastIndexOf('.');
     var ext = (pos > 0) ? request.substr(pos+1) : '';
     
-    if((request.substr(0, 1) === "/") && (ext === "html" || ext == "js" || ext == "css")) {
+    if((request.substr(0, 1) === "/") && (ext === "html")) {
+        var username = req.cookies['username'];
+        if(username === '' || username === undefined) {
+            res.render("login.html");
+        }else {
+            var page = request.substr(1, pos - 1);
+            var check_permission_res = sync_req('GET', `http://localhost:8000/api/auth/check_page_permission?username=${username}&page=${page}`);
+            if(check_permission_res.body.toString('utf-8') !== "true") {
+                page = "unauthorized";
+            }
+            res.render(page + ".html");
+        }
+    } else if((request.substr(0, 1) === "/") && (ext == "js" || ext == "css")) {
         res.render(request.substr(1));
     }else {
         next();
