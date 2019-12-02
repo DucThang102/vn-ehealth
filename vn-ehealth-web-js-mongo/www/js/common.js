173c4aafe716a278a03ee2e644d41389da72ac6c
Le Huu Hoang <lhhg87@gmail.com>
2019-12-02 09:18:30 +0700

    update file js

--- vn-ehealth-web-js-mongo/www/js/common.js
+++ vn-ehealth-web-js-mongo/www/js/common.js
@@ -1,98 +1,109 @@
-var API_URL = 'http://34.87.51.9:8001';
+var API_URL = "http://34.87.51.9:8001";
 
 Vue.mixin({
-  data: function () {
+  data: function() {
     return {
-      API_URL: 'http://34.87.51.9:8001'
-    }
+      API_URL: "http://34.87.51.9:8001"
+    };
   },
 
   methods: {
     parseDate: function(dateStr) {
-      if(dateStr != null){
+      if (dateStr != null) {
         return new Date(dateStr.substring(0, 10));
       }
       return null;
     },
 
-    getParam: function (name) {
-      if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+    getParam: function(name) {
+      if (
+        (name = new RegExp("[?&]" + encodeURIComponent(name) + "=([^&]*)").exec(
+          location.search
+        ))
+      )
         return decodeURIComponent(name[1]);
     },
 
-    serialize: function (obj) {
+    serialize: function(obj) {
       var str = [];
       for (var param in obj) {
-        str.push(encodeURIComponent(param) + "=" + encodeURIComponent(obj[param]));
+        str.push(
+          encodeURIComponent(param) + "=" + encodeURIComponent(obj[param])
+        );
       }
       return str.join("&");
     },
 
-    attr: function (obj, properties){
-      properties = properties.split('.');
-      for(let i = 0; i < properties.length; i++){
-        if(obj) obj = obj[properties[i]];
+    attr: function(obj, properties) {
+      properties = properties.split(".");
+      for (let i = 0; i < properties.length; i++) {
+        if (obj) obj = obj[properties[i]];
       }
       return obj;
     },
 
-    get: function (uri, params) {
+    get: function(uri, params) {
       var url = this.API_URL + uri;
 
       if (params) {
-        url += '?' + this.serialize(params);
+        url += "?" + this.serialize(params);
       }
 
-      console.log(`%cGET ${uri}`, 'background: blue; color: yellow', params);
+      console.log(`%cGET ${uri}`, "background: blue; color: yellow", params);
+
+      console.log(url);
 
       return fetch(url).then(response => response.json());
     },
 
-    post: function (uri, params) {
+    post: function(uri, params) {
       var url = this.API_URL + uri;
-      console.log(`%POST ${uri} : `, 'background: blue; color: yellow', params);
+      console.log(`%POST ${uri} : `, "background: blue; color: yellow", params);
 
       return fetch(url, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(params)
       }).then(response => response.json());
     },
 
-    createURL: function (htmlURL, params) {
+    createURL: function(htmlURL, params) {
       var queryStr = serialize(params);
-            return queryStr === ''? htmlURL : htmlURL + '?' + queryStr;
+      return queryStr === "" ? htmlURL : htmlURL + "?" + queryStr;
     },
 
-    callService: function (url, method, data) {
+    callService: function(url, method, data) {
       var headers = {
-        "Authorization": "Bearer " + localStorage.getItem("token")
+        Authorization: "Bearer " + localStorage.getItem("token")
       };
 
-      if (method == 'GET' || method == 'get') {
+      if (method == "GET" || method == "get") {
         return fetch(url, { headers: headers }).then(result => result.json());
       } else {
         return fetch(url, {
           headers: headers,
           body: data,
           method: method
-        })
-                .then(result => result.json());
+        }).then(result => result.json());
       }
     }
   }
-})
+});
 
-function attr(obj, properties, defaultValue){
-  properties = properties.split('.');
-  for(let i = 0; i < properties.length; i++){
-    if(obj) obj = obj[properties[i]];
+function attr(obj, properties, defaultValue) {
+  properties = properties.split(".");
+  for (let i = 0; i < properties.length; i++) {
+    if (obj) obj = obj[properties[i]];
   }
   return obj || defaultValue;
 }
 
 function getParam(name) {
-  if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+  if (
+    (name = new RegExp("[?&]" + encodeURIComponent(name) + "=([^&]*)").exec(
+      location.search
+    ))
+  )
     return decodeURIComponent(name[1]);
 }
 
@@ -105,16 +116,14 @@ function serialize(obj) {
 }
 
 function createURL(htmlURL, params) {
-    var queryStr = serialize(params);
-    return queryStr === ''? htmlURL : htmlURL + '?' + queryStr;
+  var queryStr = serialize(params);
+  return queryStr === "" ? htmlURL : htmlURL + "?" + queryStr;
 }
 
 function VueAsyncComponent(componentName, templateFile, script) {
-  Vue.component(componentName, function (resolve, reject) {
-    fetch(templateFile).then(resp => resp.text()).then(result =>
-      resolve(Object.assign({}, script, { template: result }))
-    )
+  Vue.component(componentName, function(resolve, reject) {
+    fetch(templateFile)
+      .then(resp => resp.text())
+      .then(result => resolve(Object.assign({}, script, { template: result })));
   });
 }
-
-
--- vn-ehealth-web-js-mongo/www/pages/hsba/view_detail/benh_an/benh_an.js
+++ vn-ehealth-web-js-mongo/www/pages/hsba/view_detail/benh_an/benh_an.js
@@ -1,17 +1,26 @@
 var benh_an_script = {
   data: function() {
-    return {
-      hsId: 0
-    }
+    return { hsba: null };
   },
 
   props: ["hsba_id"],
 
   computed: {
-    pdfURL : function() {
+    pdfURL: function() {
       return this.API_URL + "/api/hsba/view_pdf?hsba_id=" + this.hsba_id;
     }
-  }  
+  },
+
+  created: async function() {
+    this.hsba = await this.get("/api/hsba/get_hsba_by_id", {
+      hsba_id: this.hsba_id
+    });
+    console.log(this.hsba);
+  }
 };
 
-VueAsyncComponent('benh-an', '/pages/hsba/view_detail/benh_an/benh_an.html', benh_an_script);
+VueAsyncComponent(
+  "benh-an",
+  "/pages/hsba/view_detail/benh_an/benh_an.html",
+  benh_an_script
+);
