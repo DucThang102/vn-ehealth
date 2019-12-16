d9755c64105b05ddca2744429193d96acb590bb6
Le Huu Hoang <lhhg87@gmail.com>
2019-12-16 17:28:56 +0700

    16/12/2019

--- vn-ehealth-web-js-mongo/www/pages/hsba/view_detail/chamsoc/chamsoc.js
+++ vn-ehealth-web-js-mongo/www/pages/hsba/view_detail/chamsoc/chamsoc.js
@@ -1,8 +1,8 @@
-VueAsyncComponent('chamsoc', '/pages/hsba/view_detail/chamsoc/chamsoc.html', {
+VueAsyncComponent("chamsoc", "/pages/hsba/view_detail/chamsoc/chamsoc.html", {
   data: function() {
     return {
       chamsoc: null
-    }
+    };
   },
 
   methods: {
@@ -13,71 +13,97 @@ VueAsyncComponent('chamsoc', '/pages/hsba/view_detail/chamsoc/chamsoc.html', {
       this.chamsoc = null;
     }
   },
-  
+
   props: ["hsba_id"]
 });
 
-VueAsyncComponent('chamsoc-list', '/pages/hsba/view_detail/chamsoc/chamsoc_list.html', {
-  data: function(){
-    return {
-      chamsoc_list : null
-    }    
-  },
-
-  methods:  {
-    viewChamsoc : function(chamsoc) {
-      this.$emit('viewChamsoc', chamsoc);
+VueAsyncComponent(
+  "chamsoc-list",
+  "/pages/hsba/view_detail/chamsoc/chamsoc_list.html",
+  {
+    data: function() {
+      return {
+        chamsoc_list: null,
+        chamsoc: null,
+        hsba: null
+      };
     },
-    getNgayChamSoc : function(chamsoc) {
-      var ngayChamSocs = chamsoc.emrQuaTrinhChamSocs.map(x => x.ngaychamsoc);
-      
-      if(ngayChamSocs.length == 0) {
-        return "";
-      }
 
-      if(ngayChamSocs.length == 1) {
-        return ngayChamSocs[0];
-      }
+    methods: {
+      viewChamsoc: function(chamsoc) {
+        this.chamsoc = chamsoc;
+        $("#chamsocModal").modal();
+      },
+      getNgayChamSoc: function(chamsoc) {
+        var ngayChamSocs = chamsoc.emrQuaTrinhChamSocs.map(x =>
+          parseDate(x.ngaychamsoc)
+        );
+        ngayChamSocs = ngayChamSocs.sort(x => x.getTime());
 
-      ngayBatDau = ngayChamSocs[0];      
-      ngayKetThuc = ngayChamSocs[ngayChamSocs.length-1];
-      return "Từ " + ngayBatDau + " đến " + ngayKetThuc;
+        if (ngayChamSocs.length == 0) {
+          return "";
+        }
+        ngayBatDau = ngayChamSocs[0];
+        ngayBatDau = ngayBatDau ? ngayBatDau.substring(10) : "";
+
+        ngayKetThuc = ngayChamSocs[ngayChamSocs.length - 1];
+        ngayKetThuc = ngayKetThuc ? ngayKetThuc.substring(10) : "";
+
+        if (ngayBatDau == ngayKetThuc) {
+          return ngayBatDau;
+        } else {
+          return "Từ " + ngayBatDau + " đến " + ngayKetThuc;
+        }
+      },
+      getTenKhoa: function(khoadieutri) {
+        return khoadieutri.tenkhoa || attr(khoadieutri, "emrDmKhoaDieuTri.ten");
+      }
     },
-    getTenKhoa: function(khoadieutri){
-      return khoadieutri.tenkhoa || attr(khoadieutri, 'emrDmKhoaDieuTri.ten');
+
+    props: ["hsba_id"],
+
+    created: async function() {
+      if (this.hsba_id) {
+        this.hsba = await this.get("/api/hsba/get_hsba_by_id", {
+          hsba_id: this.hsba_id
+        });
+
+        this.chamsoc_list = await this.get("/api/chamsoc/get_ds_chamsoc", {
+          hsba_id: this.hsba_id
+        });
+        console.log(this.chamsoc_list);
+        this.chamsoc_list.forEach(x => {
+          x.ngaychamsoc = this.getNgayChamSoc(x);
+        });
+      }
     }
-  },
+  }
+);
 
-  props: ["hsba_id"],
+VueAsyncComponent(
+  "chamsoc-view",
+  "/pages/hsba/view_detail/chamsoc/chamsoc_view.html",
+  {
+    data: function() {
+      return {
+        hsba: null
+      };
+    },
+    props: ["hsba_id", "chamsoc"],
 
-  created: async function() {
-    if(this.hsba_id) {
-      this.chamsoc_list = await this.get('/api/chamsoc/get_ds_chamsoc', { hsba_id: this.hsba_id });
-      this.chamsoc_list.forEach(x => {
-        x.ngaychamsoc = this.getNgayChamSoc(x);
+    methods: {
+      viewChamsocList: function() {
+        this.$emit("viewChamsocList");
+      },
+      getTenKhoa: function(khoadieutri) {
+        return khoadieutri.tenkhoa || attr(khoadieutri, "emrDmKhoaDieuTri.ten");
+      }
+    },
+
+    created: async function() {
+      this.hsba = await this.get("/api/hsba/get_hsba_by_id", {
+        hsba_id: this.hsba_id
       });
     }
-  } 
-});
-
-VueAsyncComponent('chamsoc-view', '/pages/hsba/view_detail/chamsoc/chamsoc_view.html', {
-  data: function() {
-    return {
-      hsba: null
-    }
-  },
-  props: ["hsba_id", "chamsoc"],
-  
-  methods: {
-    viewChamsocList: function() {
-      this.$emit('viewChamsocList');
-    },
-    getTenKhoa: function(khoadieutri){
-      return khoadieutri.tenkhoa || attr(khoadieutri, 'emrDmKhoaDieuTri.ten');
-    }
-  },
-
-  created: async function() {
-    this.hsba = await this.get("/api/hsba/get_hsba_by_id", {"hsba_id": this.hsba_id});
   }
-});
+);
