76d869b878a4e3b96d769c020a180bbd0d417183
Le Huu Hoang <lhhg87@gmail.com>
2019-12-16 13:36:01 +0700

    update

--- vn-ehealth-web-js-mongo/www/pages/hsba/view_detail/chucnangsong/chucnangsong.js
+++ vn-ehealth-web-js-mongo/www/pages/hsba/view_detail/chucnangsong/chucnangsong.js
@@ -1,87 +1,114 @@
-VueAsyncComponent('chucnangsong', '/pages/hsba/view_detail/chucnangsong/chucnangsong.html', {
-  data: function() {
-    return {
-      chucnangsong: null
-    }
-  },
-
-  methods: {
-    viewChucnangsong: function(chucnangsong) {
-      this.chucnangsong = chucnangsong;
+VueAsyncComponent(
+  "chucnangsong",
+  "/pages/hsba/view_detail/chucnangsong/chucnangsong.html",
+  {
+    data: function() {
+      return {
+        chucnangsong: null
+      };
     },
-    viewChucnangsongList: function() {
-      this.chucnangsong = null;
-    }
-  },
-  
-  props: ["hsba_id"]
-});
 
-VueAsyncComponent('chucnangsong-list', '/pages/hsba/view_detail/chucnangsong/chucnangsong_list.html', {
-  data: function(){
-    return {
-      chucnangsong_list : null
-    }    
-  },
-
-  methods:  {
-    viewChucnangsong : function(chucnangsong) {
-      this.$emit('viewChucnangsong', chucnangsong);
-    },
-    getNgayTheoDoi : function(chucnangsong) {
-      var ngayTheoDois = chucnangsong.emrChucNangSongChiTiets.map(x => parseDate(x.ngaytheodoi));
-      ngayTheoDois = ngayTheoDois.sort(x => x.getTime());
-      
-      if(ngayTheoDois.length == 0) {
-        return "";
-      }
-      ngayBatDau = ngayTheoDois[0];
-      ngayBatDau = ngayBatDau? ngayBatDau.substring(10) : "";
-      
-      ngayKetThuc = ngayTheoDois[ngayTheoDois.length-1];
-      ngayKetThuc = ngayKetThuc? ngayKetThuc.substring(10) : "";
-
-      if(ngayBatDau == ngayKetThuc){
-        return ngayBatDau
-      }else{
-        return "Từ " + ngayBatDau + " đến " + ngayKetThuc;
+    methods: {
+      viewChucnangsong: function(chucnangsong) {
+        this.chucnangsong = chucnangsong;
+      },
+      viewChucnangsongList: function() {
+        this.chucnangsong = null;
       }
     },
-    getTenKhoa: function(khoadieutri){
-      return khoadieutri.tenkhoa || attr(khoadieutri, 'emrDmKhoaDieuTri.ten');
+
+    props: ["hsba_id"]
+  }
+);
+
+VueAsyncComponent(
+  "chucnangsong-list",
+  "/pages/hsba/view_detail/chucnangsong/chucnangsong_list.html",
+  {
+    data: function() {
+      return {
+        chucnangsong_list: null,
+        chucnangsong: null,
+        hsba: null
+      };
+    },
+
+    methods: {
+      viewChucnangsong: function(chucnangsong) {
+        this.chucnangsong = chucnangsong;
+        $("#chucnangsongModal").modal();
+      },
+      getNgayTheoDoi: function(chucnangsong) {
+        var ngayTheoDois = chucnangsong.emrChucNangSongChiTiets.map(x =>
+          parseDate(x.ngaytheodoi)
+        );
+        ngayTheoDois = ngayTheoDois.sort(x => x.getTime());
+
+        if (ngayTheoDois.length == 0) {
+          return "";
+        }
+        ngayBatDau = ngayTheoDois[0];
+        ngayBatDau = ngayBatDau ? ngayBatDau.substring(10) : "";
+
+        ngayKetThuc = ngayTheoDois[ngayTheoDois.length - 1];
+        ngayKetThuc = ngayKetThuc ? ngayKetThuc.substring(10) : "";
+
+        if (ngayBatDau == ngayKetThuc) {
+          return ngayBatDau;
+        } else {
+          return "Từ " + ngayBatDau + " đến " + ngayKetThuc;
+        }
+      },
+      getTenKhoa: function(khoadieutri) {
+        return khoadieutri.tenkhoa || attr(khoadieutri, "emrDmKhoaDieuTri.ten");
+      }
+    },
+
+    props: ["hsba_id"],
+
+    created: async function() {
+      if (this.hsba_id) {
+        this.hsba = await this.get("/api/hsba/get_hsba_by_id", {
+          hsba_id: this.hsba_id
+        });
+
+        this.chucnangsong_list = await this.get(
+          "/api/chucnangsong/get_ds_chucnangsong",
+          { hsba_id: this.hsba_id }
+        );
+        this.chucnangsong_list.forEach(x => {
+          x.ngaytheodoi = this.getNgayTheoDoi(x);
+        });
+        console.log(this.chucnangsong_list);
+      }
     }
-  },
+  }
+);
 
-  props: ["hsba_id"],
+VueAsyncComponent(
+  "chucnangsong-view",
+  "/pages/hsba/view_detail/chucnangsong/chucnangsong_view.html",
+  {
+    data: function() {
+      return {
+        hsba: null
+      };
+    },
+    props: ["hsba_id", "chucnangsong"],
 
-  created: async function() {
-    if(this.hsba_id) {
-      this.chucnangsong_list = await this.get('/api/chucnangsong/get_ds_chucnangsong', { hsba_id: this.hsba_id });
-      this.chucnangsong_list.forEach(x => {
-        x.ngaytheodoi = this.getNgayTheoDoi(x);
+    methods: {
+      viewChucnangsongList: function() {
+        this.$emit("viewChucnangsongList");
+      },
+      getTenKhoa: function(khoadieutri) {
+        return khoadieutri.tenkhoa || attr(khoadieutri, "emrDmKhoaDieuTri.ten");
+      }
+    },
+
+    created: async function() {
+      this.hsba = await this.get("/api/hsba/get_hsba_by_id", {
+        hsba_id: this.hsba_id
       });
     }
   }
-});
-
-VueAsyncComponent('chucnangsong-view', '/pages/hsba/view_detail/chucnangsong/chucnangsong_view.html', {
-  data: function() {
-    return {
-      hsba: null
-    }
-  },
-  props: ["hsba_id", "chucnangsong"],
-  
-  methods: {
-    viewChucnangsongList: function() {
-      this.$emit('viewChucnangsongList');
-    },
-    getTenKhoa: function(khoadieutri){
-      return khoadieutri.tenkhoa || attr(khoadieutri, 'emrDmKhoaDieuTri.ten');
-    }
-  },
-
-  created: async function() {
-    this.hsba = await this.get("/api/hsba/get_hsba_by_id", {"hsba_id": this.hsba_id});
-  }
-});
+);
--- vn-ehealth-web-js-mongo/www/pages/hsba/view_detail/donthuoc/donthuoc.js
+++ vn-ehealth-web-js-mongo/www/pages/hsba/view_detail/donthuoc/donthuoc.js
@@ -1,59 +1,83 @@
-VueAsyncComponent('donthuoc', '/pages/hsba/view_detail/donthuoc/donthuoc.html', {
-  data: function() {
-    return {
-      donthuoc: null
-    }
-  },
-
-  methods: {
-    viewDonthuoc: function(donthuoc) {
-      this.donthuoc = donthuoc;
+VueAsyncComponent(
+  "donthuoc",
+  "/pages/hsba/view_detail/donthuoc/donthuoc.html",
+  {
+    data: function() {
+      return {
+        donthuoc: null
+      };
     },
-    viewDonthuocList: function() {
-      this.donthuoc = null;
-    }
-  },
-  
-  props: ["hsba_id"]
-});
 
-VueAsyncComponent('donthuoc-list', '/pages/hsba/view_detail/donthuoc/donthuoc_list.html', {
-  data: function(){
-    return {
-      donthuoc_list : null
-    }    
-  },
-
-  methods:  {
-    viewDonthuoc : function(donthuoc) {
-      this.$emit('viewDonthuoc', donthuoc);
+    methods: {
+      viewDonthuoc: function(donthuoc) {
+        this.donthuoc = donthuoc;
+      },
+      viewDonthuocList: function() {
+        this.donthuoc = null;
+      }
     },
-  },
 
-  props: ["hsba_id"],
-
-  created: async function() {
-    if(this.hsba_id) {
-      this.donthuoc_list = await this.get('/api/donthuoc/get_ds_donthuoc', { hsba_id: this.hsba_id });
-    }
-  }  
-});
-
-VueAsyncComponent('donthuoc-view', '/pages/hsba/view_detail/donthuoc/donthuoc_view.html', {
-  data: function() {
-    return {
-      hsba: null
-    }
-  },
-  props: ["hsba_id", "donthuoc"],
-  
-  methods: {
-    viewDonthuocList: function() {
-      this.$emit('viewDonthuocList');
-    }
-  },
-
-  created: async function() {
-    this.hsba = await this.get("/api/hsba/get_hsba_by_id", {"hsba_id": this.hsba_id});
+    props: ["hsba_id"]
   }
-});
+);
+
+VueAsyncComponent(
+  "donthuoc-list",
+  "/pages/hsba/view_detail/donthuoc/donthuoc_list.html",
+  {
+    data: function() {
+      return {
+        donthuoc_list: null,
+        donthuoc: null,
+        hsba: null
+      };
+    },
+
+    methods: {
+      viewDonthuoc: function(donthuoc) {
+        this.donthuoc = donthuoc;
+        $("#donthuocModal").modal();
+      }
+    },
+
+    props: ["hsba_id"],
+
+    created: async function() {
+      if (this.hsba_id) {
+        this.hsba = await this.get("/api/hsba/get_hsba_by_id", {
+          hsba_id: this.hsba_id
+        });
+
+        this.donthuoc_list = await this.get("/api/donthuoc/get_ds_donthuoc", {
+          hsba_id: this.hsba_id
+        });
+        console.log(this.donthuoc_list);
+      }
+    }
+  }
+);
+
+VueAsyncComponent(
+  "donthuoc-view",
+  "/pages/hsba/view_detail/donthuoc/donthuoc_view.html",
+  {
+    data: function() {
+      return {
+        hsba: null
+      };
+    },
+    props: ["hsba_id", "donthuoc"],
+
+    methods: {
+      viewDonthuocList: function() {
+        this.$emit("viewDonthuocList");
+      }
+    },
+
+    created: async function() {
+      this.hsba = await this.get("/api/hsba/get_hsba_by_id", {
+        hsba_id: this.hsba_id
+      });
+    }
+  }
+);
