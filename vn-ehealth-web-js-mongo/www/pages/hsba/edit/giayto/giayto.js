c73cd737d4fb7b7c494e8d78c46543591baac348
lethiduyen000097 <duyenlt060097@gmail.com>
2020-01-08 17:43:29 +0700

    Sua giay to

--- vn-ehealth-web-js-mongo/www/pages/hsba/edit/giayto/giayto.js
+++ vn-ehealth-web-js-mongo/www/pages/hsba/edit/giayto/giayto.js
@@ -1,7 +1,14 @@
-VueAsyncComponent('giayto', '/pages/hsba/edit/giayto/giayto.html', {
-  data: function () {
+VueAsyncComponent('giayto', '/pages/hsba/view_detail/giayto/giayto.html', {
+  data: function() {
     return {
-      giayto_list: [],
+      hsba: null
     }
   },
+
+  props: ["hsba_id"],
+
+  created: async function() {
+    this.hsba = await this.get("/api/hsba/get_hsba_by_id", {"hsba_id": this.hsba_id});
+  }
+
 });
