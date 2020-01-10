472c998c9bbc5e0a59699a1086777e079025387a
lethiduyen000097 <duyenlt060097@gmail.com>
2020-01-10 15:57:28 +0700

    bug button luu

--- vn-ehealth-web-js-mongo/www/pages/hsba/edit/giayto/giayto.js
+++ vn-ehealth-web-js-mongo/www/pages/hsba/edit/giayto/giayto.js
@@ -1,10 +1,12 @@
 VueAsyncComponent('giayto', '/pages/hsba/edit/giayto/giayto.html', {
   data: function () {
     return {
-      hsba: null
+      hsba: null,
+      perPage: 10,
+    	currentPage: 1,
+    
     }
   },
-  
   props: ["hsba_id"],
 
   methods: {
@@ -16,15 +18,32 @@ VueAsyncComponent('giayto', '/pages/hsba/edit/giayto/giayto.html', {
       var response = await fetch( this.API_URL + '/api/hsba/add_giayto',
         { body: formData, method: 'POST' });
       var result = await response.json();
-      alert(result.success);
+      //alert(result.success);
       console.log(result);
-
       this.hsba = await this.get("/api/hsba/get_hsba_by_id", {"hsba_id": this.hsba_id});
+      $('#upload').modal('hide');
+    },
+
+    deleteFileDinhKem: function(index) {
+      this.hsba.emrFileDinhKems.splice(index, 1);
+    },
+    saveFileDinhKem: async function() {
+      var result = await this.post("/api/FileDinhKem/create_or_update_FileDinhKem", this.FileDinhKem);
+      if(result.success) {
+        sessionStorage.removeItem('dataChange');
+        this.$emit('hsba.emrFileDinhKems');
+      }else {
+        alert('Lỗi xảy ra quá trình lưu thông tin');
+      }
+    },
+      },
+
+  watch: {
+    currentPage: async function() {
+      await this.getData();
     }
   },
   created: async function() {
     this.hsba = await this.get("/api/hsba/get_hsba_by_id", {"hsba_id": this.hsba_id});
-    console.log(this.hsba);
   }
-
 });
--- vn-ehealth-web-js-mongo/www/pages/hsba/view_detail/giayto/giayto.js
+++ vn-ehealth-web-js-mongo/www/pages/hsba/view_detail/giayto/giayto.js
@@ -1,14 +1,49 @@
 VueAsyncComponent('giayto', '/pages/hsba/view_detail/giayto/giayto.html', {
-  data: function() {
+
+  data: function () {
     return {
-      hsba: null
+      hsba: null,
+      perPage: 10,
+    	currentPage: 1,
+    
     }
   },
-
   props: ["hsba_id"],
 
+  methods: {
+    openUploadModal: function() {
+      $('#upload').modal();
+    },
+    upload: async function() {
+      var formData = new FormData(document.getElementById("fmt"));
+      var response = await fetch( this.API_URL + '/api/hsba/add_giayto',
+        { body: formData, method: 'POST' });
+      var result = await response.json();
+      //alert(result.success);
+      console.log(result);
+      this.hsba = await this.get("/api/hsba/get_hsba_by_id", {"hsba_id": this.hsba_id});
+      $('#upload').modal('hide');
+    },
+
+    deleteFileDinhKem: function(index) {
+      this.hsba.emrFileDinhKems.splice(index, 1);
+    },
+    saveFileDinhKem: async function() {
+      var result = await this.post("/api/FileDinhKem/create_or_update_FileDinhKem", this.FileDinhKem);
+      if(result.success) {
+        sessionStorage.removeItem('dataChange');
+        this.$emit('hsba.emrFileDinhKems');
+      }else {
+        alert('Lỗi xảy ra quá trình lưu thông tin');
+      }
+    }
+  },
+  watch: {
+  currentPage: async function() {
+    await this.getData();
+  }
+  },
   created: async function() {
     this.hsba = await this.get("/api/hsba/get_hsba_by_id", {"hsba_id": this.hsba_id});
   }
-
 });
