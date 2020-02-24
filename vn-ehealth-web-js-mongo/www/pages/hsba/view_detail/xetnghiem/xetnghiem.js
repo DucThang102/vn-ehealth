c846361da73459f9a2c10c11b815c0e1a0d2b369
Le Huu Hoang <lhhg87@gmail.com>
2020-02-24 12:04:00 +0700

    update xetnghiem_list.html

--- vn-ehealth-web-js-mongo/www/pages/hsba/view_detail/xetnghiem/xetnghiem.js
+++ vn-ehealth-web-js-mongo/www/pages/hsba/view_detail/xetnghiem/xetnghiem.js
@@ -1,18 +1,17 @@
 VueAsyncComponent(
   "xetnghiem",
-  "/pages/hsba/view_detail/xetnghiem/xetnghiem.html",
-  {
-    data: function() {
+  "/pages/hsba/view_detail/xetnghiem/xetnghiem.html", {
+    data: function () {
       return {
         xetnghiem: null
       };
     },
 
     methods: {
-      viewXetnghiem: function(xetnghiem) {
+      viewXetnghiem: function (xetnghiem) {
         this.xetnghiem = xetnghiem;
       },
-      viewXetnghiemList: function() {
+      viewXetnghiemList: function () {
         this.xetnghiem = null;
       }
     },
@@ -23,9 +22,8 @@ VueAsyncComponent(
 
 VueAsyncComponent(
   "xetnghiem-list",
-  "/pages/hsba/view_detail/xetnghiem/xetnghiem_list.html",
-  {
-    data: function() {
+  "/pages/hsba/view_detail/xetnghiem/xetnghiem_list.html", {
+    data: function () {
       return {
         xetnghiem_list: null,
         xetnghiem: null,
@@ -34,29 +32,48 @@ VueAsyncComponent(
     },
 
     methods: {
-      viewXetnghiem: function(xetnghiem) {
+      viewXetnghiem: function (xetnghiem) {
         this.xetnghiem = xetnghiem;
         $("#xetnghiemModal").modal();
-      }
+      },
+
+      formatNgay2(ngaygio) {
+        if (ngaygio.length >= 10) {
+          var ngay = ngaygio.substring(0, 2);
+          var thang = ngaygio.substring(3, 5);
+          var nam = ngaygio.substring(6, 10);
+          return `Ngày ${ngay} tháng ${thang} năm ${nam}`;
+        }
+        return "Ngày ... tháng ... năm ......";
+      },
+      getTenKhoa(khoa) {
+        return khoa.tenkhoa || attr(khoa, "emrDmKhoaDieuTri.ten");
+      },
+      khoaDieuTri() {
+        if (this.hsba.emrVaoKhoas.length > 0) {
+          var n = this.hsba.emrVaoKhoas.length;
+          return this.hsba.emrVaoKhoas[n - 1];
+        }
+        return {};
+      },
     },
 
     props: ["hsba_id"],
 
-    created: async function() {
+    created: async function () {
       if (this.hsba_id) {
         this.hsba = await this.get("/api/hsba/get_hsba_by_id", {
           hsba_id: this.hsba_id
         });
 
         this.xetnghiem_list = await this.get(
-          "/api/xetnghiem/get_ds_xetnghiem",
-          {
+          "/api/xetnghiem/get_ds_xetnghiem", {
             hsba_id: this.hsba_id
           }
         );
         this.xetnghiem_list.forEach(xn => {
-          xn.emrXetNghiemDichVus = xn.emrXetNghiemDichVus.filter(xndv => 
-              xndv.emrXetNghiemKetQuas.length > 0);
+          xn.emrXetNghiemDichVus = xn.emrXetNghiemDichVus.filter(xndv =>
+            xndv.emrXetNghiemKetQuas.length > 0);
         });
       }
     }
@@ -65,9 +82,8 @@ VueAsyncComponent(
 
 VueAsyncComponent(
   "xetnghiem-view",
-  "/pages/hsba/view_detail/xetnghiem/xetnghiem_view.html",
-  {
-    data: function() {
+  "/pages/hsba/view_detail/xetnghiem/xetnghiem_view.html", {
+    data: function () {
       return {
         hsba: null
       };
@@ -75,12 +91,12 @@ VueAsyncComponent(
     props: ["hsba_id", "xetnghiem"],
 
     methods: {
-      viewXetnghiemList: function() {
+      viewXetnghiemList: function () {
         this.$emit("viewXetnghiemList");
       }
     },
 
-    created: async function() {
+    created: async function () {
       this.hsba = await this.get("/api/hsba/get_hsba_by_id", {
         hsba_id: this.hsba_id
       });
