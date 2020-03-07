68c7c0ff958304ac9bb0c4b4607326b792d941ed
tungdt <duongthanhtungvn01@gmail.com>
2020-03-07 15:31:01 +0700

    Update

--- vn-ehealth-service-mongo/src/main/java/vn/ehealth/emr/model/EmrGiaiPhauBenh.java
+++ vn-ehealth-service-mongo/src/main/java/vn/ehealth/emr/model/EmrGiaiPhauBenh.java
@@ -43,6 +43,8 @@ public class EmrGiaiPhauBenh {
     public String nhanxetdaithe;
     public String nhanxetvithe;
     public String motachandoangiaiphau;
+    
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     public Date ngaylaymausinhthiet;
         
     public List<EmrFileDinhKem> emrFileDinhKemGpbs = new ArrayList<>();
