ff8cf7f306421791d1014d0b7886a64a8765acca
DucThang102 <33811910+DucThang102@users.noreply.github.com>
2020-03-12 23:23:03 +0700

    config mail sender

--- vn-ehealth-service-mongo/src/main/java/vn/ehealth/emr/controller/UserController.java
+++ vn-ehealth-service-mongo/src/main/java/vn/ehealth/emr/controller/UserController.java
@@ -10,10 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import vn.ehealth.emr.model.EmrPerson;
 import vn.ehealth.emr.service.UserService;
@@ -22,64 +19,56 @@ import vn.ehealth.emr.validate.Validator;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-    
-	private Logger logger = LoggerFactory.getLogger(UserController.class);
-    
-    @Autowired UserService userService;
-    
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    UserService userService;
+
     @PostMapping("/create_user")
-    public ResponseEntity<?> createUser(@RequestParam("emr_co_so_kham_benh_id") String emrCoSoKhamBenhId, @RequestParam("emrPerson") EmrPerson emrPerson, @RequestParam("password") String password, @RequestParam("role_ids") List<String> roleIds) {
+    public ResponseEntity<?> createUser(@RequestParam("emr_co_so_kham_benh_id") String emrCoSoKhamBenhId, @RequestParam("emrPerson") EmrPerson emrPerson, @RequestParam("role_ids") List<String> roleIds) {
         try {
-        	var error = Validator.isValidPassword(password);
-        	if (!error.equals("")) {
-        		var result = Map.of(
-                        "success" , false,
-                        "error", error 
-                    );
-        		return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
-        	}
-        	
-        	 if(!emrCoSoKhamBenhId.isEmpty() && !Validator.isCheckTyeObjectId(emrCoSoKhamBenhId)) {
-        		 var result = Map.of(
-                         "success" , false,
-                         "error", "emr_co_so_kham_benh_id định dạng không đúng objectID" 
-                     );
-        		 return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
-        	 }
-        	 if(!Validator.isValidRequier(emrPerson.email)) {
-        		 var result = Map.of(
-                         "success" , false,
-                         "error", "emrPerson.email là bắt buộc" 
-                     );
-        		 return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
-        	 }
-        	 if(!Validator.isValidRequier(emrPerson.tendaydu)) {
-        		 var result = Map.of(
-                         "success" , false,
-                         "error", "emrPerson.tendaydu là bắt buộc" 
-                     );
-        		 return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
-        	 }
-        
-            var user = userService.createUser(new ObjectId(emrCoSoKhamBenhId), emrPerson, password, roleIds);
-            
+            if (!emrCoSoKhamBenhId.isEmpty() && !Validator.isCheckTyeObjectId(emrCoSoKhamBenhId)) {
+                var result = Map.of(
+                        "success", false,
+                        "error", "emr_co_so_kham_benh_id định dạng không đúng objectID"
+                );
+                return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+            }
+            if (!Validator.isValidRequier(emrPerson.email)) {
+                var result = Map.of(
+                        "success", false,
+                        "error", "emrPerson.email là bắt buộc"
+                );
+                return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+            }
+            if (!Validator.isValidRequier(emrPerson.tendaydu)) {
+                var result = Map.of(
+                        "success", false,
+                        "error", "emrPerson.tendaydu là bắt buộc"
+                );
+                return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+            }
+
+            var user = userService.createUser(new ObjectId(emrCoSoKhamBenhId), emrPerson, roleIds);
+
             var result = Map.of(
-                "success" , true,
-                "user" , user
-               
+                    "success", true,
+                    "user", user
+
             );
-            
+
             return ResponseEntity.ok(result);
-            
-        }catch(Exception e) {
+
+        } catch (Exception e) {
             var error = Optional.ofNullable(e.getMessage()).orElse("Unknown error");
             var result = Map.of(
-                "success" , false,
-                "error", error 
+                    "success", false,
+                    "error", error
             );
             logger.error("Error save user: ", e);
             return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
         }
     }
-  
+
 }
--- vn-ehealth-service-mongo/src/main/java/vn/ehealth/emr/service/UserService.java
+++ vn-ehealth-service-mongo/src/main/java/vn/ehealth/emr/service/UserService.java
@@ -1,11 +1,5 @@
 package vn.ehealth.emr.service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import javax.annotation.Nonnull;
-
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,8 +9,16 @@ import vn.ehealth.emr.model.EmrPerson;
 import vn.ehealth.emr.model.User;
 import vn.ehealth.emr.repository.UserRepository;
 
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class UserService {
+
+	@Autowired private EmailService emailService;
 
     @Autowired UserRepository userRepository;
     
@@ -30,7 +32,7 @@ public class UserService {
         return userRepository.findByUsername(username);
     }
     
-    public User createUser(@Nonnull ObjectId emrCoSoKhamBenhId, @Nonnull EmrPerson emrPerson, String password, List<String> roleIds) { 
+    public User createUser(@Nonnull ObjectId emrCoSoKhamBenhId, @Nonnull EmrPerson emrPerson, List<String> roleIds) {
     	var user = userRepository.findByUsername(emrPerson.email).orElse(null);
     	user.emrCoSoKhamBenhId = emrCoSoKhamBenhId;
     	user.fullName = emrPerson.tendaydu;
@@ -44,9 +46,22 @@ public class UserService {
     	user.emrPersonId = emrPerson.id;
     	user.username = emrPerson.email;
     	PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-    	user.password =  passwordEncoder.encode(password);
+		UUID uuid =  UUID.randomUUID();
+		String randomPassword = uuid.toString();
+    	user.password =  passwordEncoder.encode(randomPassword);
     	
     	user = userRepository.save(user);
+		sendMailNotifyAccountInfo(emrPerson.email, emrPerson.tendaydu, randomPassword);
         return user;
     }
+
+	public void sendMailNotifyAccountInfo(String email, String name, String password) {
+		String content = "Xin chào " + name
+				+ "Đây là thông tin tài khoản của bạn: \n"
+				+ "username: " + email
+				+ "\npassword: " + password
+				+ "\nVui lòng không cung cấp cho bất kỳ ai để bảo vệ tài khoản của bạn.\n";
+		emailService.sendEmail(email,"Thông báo thông tin tài khoản", content);
+	}
+
 }
